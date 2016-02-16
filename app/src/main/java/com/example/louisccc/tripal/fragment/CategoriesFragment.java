7697f7bc64913b61ebbd3d0ff8abe7fe0a308c51
louisccc <louis29418401@gmail.com>
2016-02-16 21:05:06 +0800

    fix runtime error in categoryfragment

--- app/src/main/java/com/example/louisccc/tripal/fragment/CategoriesFragment.java
+++ app/src/main/java/com/example/louisccc/tripal/fragment/CategoriesFragment.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import com.example.louisccc.tripal.R;
 import com.example.louisccc.tripal.model.TriApplication;
 import com.example.louisccc.tripal.model.TriFriend;
+import com.example.louisccc.tripal.utility.FriendsAdapter;
 
 import java.util.ArrayList;
 
@@ -53,25 +54,5 @@ public class CategoriesFragment extends Fragment {
         mListview.addHeaderView(textview, null, false);
 
         mListViewAdapter.notifyDataSetChanged();
-    }
-
-    private class FriendsAdapter extends ArrayAdapter<TriFriend> {
-        private ArrayList<TriFriend> mFriends;
-        private int mResrc_id;
-        private LayoutInflater mVi;
-
-        public FriendsAdapter(Context ctx, int resrc_id, ArrayList<TriFriend> friends) {
-            super( ctx, resrc_id, friends );
-            this.mFriends = friends;
-            this.mResrc_id = resrc_id;
-            this.mVi = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        }
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            convertView = mVi.inflate(mResrc_id, null);
-            TextView text = (TextView) convertView.findViewById(R.id.ItemDesc);
-            text.setText(mFriends.get(position).getName());
-            return convertView;
-        }
     }
 }
